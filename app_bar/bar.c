2d70e4537d7e8df5ca2b3d334d8c5c8fd800077e
Chen Qi <Qi.Chen@windriver.com>
2021-12-15 11:07:20 +0800

    init LAT examples
    Signed-off-by: Chen Qi <Qi.Chen@windriver.com>

---
+++ app_bar/bar.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <foo.h>
+
+int main(void) {
+	printf("This is bar invoking foo.\n");
+	foo();
+	return 0;
+}
---
+++ lib_foo/foo.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void foo(void) {
+	printf("This is foo function in libfoo\n");
+}
